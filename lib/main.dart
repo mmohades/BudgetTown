a5373f722ebcaf2f1e728997c37474d76fac3820
Kevin Kannammalil <kevinrk29@hotmail.com>
2019-10-19 10:03:27 -0400

    made pages folder and seperated home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import './pages/home_page.dart';
 import 'package:flutter/material.dart';
-
-var _counterD = Firestore.instance.collection('counters').document('counter');
 
 void main() => runApp(MyApp());
 
@@ -15,66 +13,8 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
+      // home: new LandingPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    _getCounterFromFirebase();
-  }
-
-  Future<void> _getCounterFromFirebase() async {
-    var n = await _counterD.get();
-    _counter = n.data['counter'] ?? 0;
-  }
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-      _counterD.setData({
-        'counter': _counter,
-      }, merge: true);
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+var _counterD = Firestore.instance.collection('counters').document('counter');
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _getCounterFromFirebase();
+  }
+
+  Future<void> _getCounterFromFirebase() async {
+    var n = await _counterD.get();
+    _counter = n.data['counter'] ?? 0;
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+      _counterD.setData({
+        'counter': _counter,
+      }, merge: true);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
