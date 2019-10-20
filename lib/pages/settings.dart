f9fa421aebccce05d0d1d03138d3ae6e8d02d19b
Kevin Kannammalil <kevinrk29@hotmail.com>
2019-10-19 21:59:33 -0400

    created Settings Columns

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -2,11 +2,43 @@ import 'package:flutter/material.dart';
 
 class Settings extends StatelessWidget {
   const Settings({Key key}) : super(key: key);
+
   @override 
   Widget build(BuildContext context){
-    return Text(
-      'Index 2: Settings',
-      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Settings')
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 50),
+            // Expanded(
+            //   child: _options(context),
+            // ),
+          ],
+        ),
+      ),
     );
   }
+
+  // Widget _options(BuildContext context){
+  //   return ListView(
+  //     children: <Widget>[
+  //       _
+  //     ],
+  //   );
+  // }
+
+  // Widget _optionsListTitle(
+  //   String title,
+  //   Icon icon, 
+  //   Color color,
+  //   VoidCallback onTap,
+  // ) {
+  //   return ListTile(
+
+  //   )
+  // }
 }
